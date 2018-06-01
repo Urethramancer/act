c0836ea413eb8bf90b99fa6144790b55b97508b1
Ronny Bangsund <orb@neuralshortcircuit.com>
2018-06-02 00:00:00 +0200

    List all entries.

---
+++ cmd-all.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"github.com/Urethramancer/cross"
+)
+
+type AllCmd struct {
+}
+
+func (ac *AllCmd) Execute(args []string) error {
+	list, err := AllPaths()
+	if err != nil {
+		return err
+	}
+
+	for _, e := range list {
+		path := filepath.Join(cross.ConfigPath(), e)
+		act, err := LoadActions(path)
+		if err != nil {
+			return err
+		}
+		name := strings.TrimSuffix(e, ".json")
+		pr("%s:", name)
+		var a []string
+		for k, v := range act.List {
+			s := fmt.Sprintf("%s: %s", k, v)
+			a = append(a, s)
+		}
+		sort.Strings(a)
+		for _, x := range a {
+			pr("%s", x)
+		}
+	}
+	return nil
+}
