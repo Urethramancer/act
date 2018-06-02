466feeecf4922e58c053c78c5a18c3487c007470
Ronny Bangsund <orb@neuralshortcircuit.com>
2018-06-02 18:12:17 +0200

    Add implemented.

--- cmd-add.go
+++ cmd-add.go
@@ -1,11 +1,28 @@
 package main
 
+import (
+	"os"
+	"time"
+)
+
 // AddCmd tool command.
 type AddCmd struct {
-	Action string
+	Action struct {
+		Text []string `positional-arg-name:"TEXT" description:"Action text in quotes."`
+	} `positional-args:"yes"`
 }
 
 // Execute add.
 func (ac *AddCmd) Execute(args []string) error {
+	if len(ac.Action.Text) == 0 {
+		pr("You need to specify an action to add.")
+		os.Exit(1)
+	}
+
+	act := loadCurrentOrFail()
+	act.Counter++
+	act.List[act.Counter] = ac.Action.Text[0]
+	act.LastModified = time.Now().Local()
+	SaveActions(act)
 	return nil
 }
