8a03a0e9a523989d8ad360d12b92ea7b51e5de89
Ronny Bangsund <orb@neuralshortcircuit.com>
2018-06-02 18:13:39 +0200

    Added the convenience function loadCurrentOrFail().

--- utility.go
+++ utility.go
@@ -15,3 +15,13 @@ func check(e error) {
 		os.Exit(2)
 	}
 }
+
+func loadCurrentOrFail() *Actions {
+	cwd, err := os.Getwd()
+	check(err)
+	fname, err := PathToFile(cwd)
+	check(err)
+	act, err := LoadActions(fname)
+	check(err)
+	return act
+}
