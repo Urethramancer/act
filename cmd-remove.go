300e49d2bd0fdf5d278e04b609c0910ac4df64de
Ronny Bangsund <orb@neuralshortcircuit.com>
2018-06-02 21:09:27 +0200

    Remove implemented.

--- cmd-remove.go
+++ cmd-remove.go
@@ -1,13 +1,37 @@
 package main
 
+import "fmt"
+
 // RemoveCmd tool command.
 type RemoveCmd struct {
-	Pos struct {
-		IDs []string
+	Args struct {
+		IDs []uint64 `positional-arg-name:"ACTION" description:"Action to remove."`
 	} `positional-args:"yes"`
 }
 
 // Execute remove.
 func (rc *RemoveCmd) Execute(args []string) error {
-	return nil
+	if len(rc.Args.IDs) == 0 {
+		pr("You need to specify one or more action IDs to remove.")
+		return nil
+	}
+
+	act := loadCurrentOrFail()
+	x := true
+	for _, id := range rc.Args.IDs {
+		_, ok := act.List[id]
+		if ok {
+			delete(act.List, id)
+			if x == false {
+				fmt.Printf(", %d", id)
+			} else {
+				fmt.Printf("Removed %d", id)
+				x = false
+			}
+		} else {
+			pr("\nUnknown ID %d", id)
+		}
+	}
+	pr("")
+	return act.Save()
 }
